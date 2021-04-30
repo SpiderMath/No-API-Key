4c0130fe39ce36a507763ac98e96b9249ef1bf42
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-30 21:21:21 +0530

    Added Custom Logger

--- Source/server.ts
+++ Source/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import Logger from "./Helpers/Logger";
 
 const app = express();
 
 const port = 6969;
 
-app.listen(port, () => console.log(`Listening to API Calls on port ${port}!`));
+app.listen(port, () => Logger.success("server", `Listening to API Calls on port ${port}!`));
 
 app.get("/", (req, res) => {
 	res.send("<b>Hello World!</b>");
