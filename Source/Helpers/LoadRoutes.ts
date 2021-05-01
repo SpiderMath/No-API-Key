427b17fda75da25d444d6553e53723f2e70c894d
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-01 09:53:40 +0530

    Added Interface for RouteExport :thinking:

--- Source/Helpers/LoadRoutes.ts
+++ Source/Helpers/LoadRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { readdirSync } from "fs";
 import { join } from "path";
+import RouteExport from "../Constants/RouteExport";
 import Logger from "./Logger";
 
 export default function LoadRoutes(folderName: string, router: Router, routerName: string) {
@@ -9,7 +10,7 @@ export default function LoadRoutes(folderName: string, router: Router, routerNam
 		.forEach(async (fileName: string) => {
 			const pseudoPull = await import(join(__dirname, `../Routes/${folderName}/${fileName}`));
 
-			const pull = pseudoPull.default;
+			const pull: RouteExport = pseudoPull.default;
 
 			router.use(`/${pull.name.toLowerCase()}`, pull.run);
 			Logger.success(`server/routes/${routerName}`, `Loaded /${pull.name.toLowerCase()}`);
--- Source/Routes/Image/Export.ts
+++ Source/Routes/Image/Export.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from "express";
+import RouteExport from "../../Constants/RouteExport";
 
-const e = {
-	name: "e",
-	run(req: Request, res: Response) {
-		res.send("e");
+const WideImage: RouteExport = {
+	name: "wideimage",
+	description: "WIDE IMAGE POG",
+	async run(req, res) {
+		res.send("<h1>Hello World</h1>");
 	},
+	params: [],
 };
 
-export default e;
+export default WideImage;
