a9eb5a1fd468fbae296bb5ab8ef3b06fe9df2d6a
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-18 12:49:57 +0530

    Removed everything, time to turn a new leaf 🍃

--- Source/Constants/ConsoleColours.ts
+++
@@ -1,9 +0,0 @@
-const ConsoleColours = {
-	red : "\x1b[31m",
-	green : "\x1b[32m",
-	yellow : "\x1b[33m",
-	blue : "\x1b[34m",
-	reset : "\u001b[0m",
-};
-
-export { ConsoleColours };
--- Source/Constants/RouterExport.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-export default interface RouterExport {
-	router: Router,
-	name: string,
-// eslint-disable-next-line semi
-}
