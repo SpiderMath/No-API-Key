e4b4edb3a71909afc234924eded18d5976e532c1
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-26 22:36:23 +0530

    SUCCESSFULLY ADDED SUBROUTE!!!!!!!!!! :tada:

--- Source/Packages/Subdomain.ts
+++ Source/Packages/Subdomain.ts
@@ -3,8 +3,11 @@ import { Application, Router } from "express";
 export default function Subdomain(subdomainName: string, router: Router, app: Application) {
 	app.use((req, res, next) => {
 		if(req.subdomains.length && (req.subdomains[0].toLowerCase() === subdomainName.toLowerCase())) {
+			app.use(router);
+			next();
+		}
+		else {
 			next();
 		}
 	});
-	app.use("/", router);
 }
