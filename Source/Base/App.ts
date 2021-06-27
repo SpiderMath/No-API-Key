110ffa7fc61ab55afb98298aff1be410cf9f7623
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-27 09:10:35 +0530

    Moved to API stuff to API Router

--- Source/Base/App.ts
+++ Source/Base/App.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Router } from "express";
 import { appendFile, readdir } from "fs/promises";
 import { join } from "path";
 import BaseRoute from "./BaseRoute";
@@ -25,6 +25,7 @@ export class App {
 	async _loadRoutes(routesDir: string) {
 		const subDirs = await readdir(routesDir);
 		const subRouteList: any = await this.util.readJSON(join(__dirname, "../../Assets/JSON/APISubroutes.json"));
+		const APIRouter = Router();
 
 		for(const subDir of subDirs) {
 			const files = await readdir(join(routesDir, subDir));
@@ -35,7 +36,7 @@ export class App {
 				const pull: BaseRoute = new pseudoPull.default(this);
 				const APISubRoute = subRouteList[subDir.toLowerCase()] || subDir.toLowerCase();
 
-				this.main.get(`/${APISubRoute}/${pull.name}`, async (req, res, next) => {
+				APIRouter.get(`/${APISubRoute}/${pull.name}`, async (req, res, next) => {
 					if(pull.adminOnly && (!req.query.key || req.query.key !== this.adminKey)) return this.util.badRequest(res, "This is an admin only endpoint!");
 
 					pull.run(req, res, next);
@@ -44,5 +45,7 @@ export class App {
 				this.logger.success("server/routes", `Loaded Route /${APISubRoute}/${pull.name} successfully!`);
 			}
 		}
+
+		this.main.use("/api", APIRouter);
 	}
 };
